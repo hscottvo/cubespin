4b356d75cc0bec142f074a36dfd97a52c04b856e
hscottvo <56327086+hscottvo@users.noreply.github.com>
2024-11-22 01:25:31 -0800

    Add 2d rotation

--- util/geometry.go
+++ util/geometry.go
@@ -16,3 +16,25 @@ func (a Angle) Add(r float64) {
 	a.r = math.Mod(a.r+r, 360)
 
 }
+
+type Vec2 struct {
+	X float64
+	Y float64
+}
+
+func (v Vec2) Rotate2D(a Angle, pX float64, pY float64) {
+	aboutOrigin := pX != 0 || pY != 0
+	if aboutOrigin {
+		v.X -= pX
+		v.Y -= pY
+	}
+
+	v.X = v.X*math.Cos(a.r) + v.X*math.Sin(a.r)
+	v.Y = v.Y*math.Cos(a.r) + -1*v.Y*math.Sin(a.r)
+
+	if aboutOrigin {
+		v.X += pX
+		v.Y += pY
+
+	}
+}
